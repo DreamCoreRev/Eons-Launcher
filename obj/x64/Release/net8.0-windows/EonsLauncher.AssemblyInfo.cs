21704e26309eec18a51f838496cc65a4942d483a
Aurøra <azgath.gaming@gmail.com>
2026-04-23 04:57:10 +0200

    Update Icon

--- obj/x64/Release/net8.0-windows/EonsLauncher.AssemblyInfo.cs
+++ obj/x64/Release/net8.0-windows/EonsLauncher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EonsLauncher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+647e5cb075a5ed4d91c5eb397fd33b41216f63af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279051fa56e14bf429c3f114d51022b6844ebe92")]
 [assembly: System.Reflection.AssemblyProductAttribute("EonsLauncher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EonsLauncher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
